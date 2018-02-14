f9eb7ceeba55e0efbd6cca4091bf8accca2184cc
doyleliu <doyleliu96@gmail.com>
2018-02-14 15:22:24 +0800

    first test

---
+++ cmake-build-debug/CMakeFiles/eye_tracking.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eye_tracking.dir/main.cpp.o"
+  "eye_tracking.pdb"
+  "eye_tracking"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/eye_tracking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
